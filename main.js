fbc6ff8172f5f2a26a94e36042593c0728ca7c8d
elyessKaroui <elyess.karoui@esprit.tn>
2023-12-17 17:56:25 +0100

    partie jss

---
+++ main.js
@@ -0,0 +1,97 @@
+
+
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+let two = document.querySelector(".two");
+
+window.onload = function () {
+  two.focus();
+};
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    if (validateInputs()) {
+        alert('Vos données sont valides!');
+    }
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else if (!/^[a-zA-Z]+$/.test(usernameValue)) {
+        setError(username, 'Username can only contain alphabets (uppercase or lowercase)');
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 characters.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords don't match");
+    } else {
+        setSuccess(password2);
+    }
+
+    return (
+        usernameValue !== '' &&
+        /^[a-zA-Z]+$/.test(usernameValue) &&
+        emailValue !== '' &&
+        isValidEmail(emailValue) &&
+        passwordValue !== '' &&
+        passwordValue.length >= 8 &&
+        password2Value !== '' &&
+        password2Value === passwordValue
+    );
+    
+};
+
+  
+
